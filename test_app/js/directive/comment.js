86ebd014f78433c44ec988d09f3aaa50ae087b41
kmeddah <khaled.meddah@ymail.com>
2020-02-09 20:48:50 +0100

    Ajout de la directive time

--- test_app/js/directive/comment.js
+++ test_app/js/directive/comment.js
@@ -8,3 +8,25 @@ app.directive('ngComment', function () {
         templateUrl: 'partials/_comment.html'
     }
 })
+
+app.directive('time', function (dateFilter, $interval) {
+    return {
+        restrict : 'E',
+        template : '{{time}}',
+        scope : {
+
+        },
+        link: function (scope, element, attrs){
+            scope.time = dateFilter(new Date(), 'hh:mm:ss');
+
+            element.on('destroy', function() {
+                $interval.cancel(interval);
+            })
+
+            interval = $interval(function () {
+                scope.time = dateFilter(new Date(), 'hh:mm:ss');
+                console.log('time changed');
+            }, 1000)
+        }
+    }
+})
